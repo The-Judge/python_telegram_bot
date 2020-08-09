5cd80df5d6a23881fa10e5c8b483a9ca1464de29
Marc Richter <marc.richter@henkel.com>
2020-08-09 22:53:33 +0200

    Adding __str__ definition for User Model

--- bot/models.py
+++ bot/models.py
@@ -5,3 +5,6 @@ class User(models.Model):
     user_id     = models.IntegerField(unique=True, primary_key=True)
     first_name  = models.CharField(max_length=64)
     last_name   = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'

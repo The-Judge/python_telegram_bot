a2df4b4f969a14d601a513a47f0031fd0c4efce0
Marc Richter <marc.richter@henkel.com>
2020-08-09 22:51:58 +0200

    Adding staticfiles/ to .gitignore Created Django User Model Register User Model to Django Admin Adding User Model migration

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_id     = models.IntegerField(unique=True, primary_key=True)
+    first_name  = models.CharField(max_length=64)
+    last_name   = models.CharField(max_length=64)
